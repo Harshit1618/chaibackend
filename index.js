2fd1dd85c0347e4b6c11fc34e58e255f9c5e161f
Harshith150 <katukamharshith150@gmail.com>
2024-11-03 16:04:44 +0530

    added /home

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/',(req,res)=>{
     res.send("hello")
 })
 
+app.get('/home',(req,res)=>{
+    res.send("in hitesh chowdary backend")
+})
+
 app.listen(process.env.PORT,()=>{
     console.log("server started")
 })
